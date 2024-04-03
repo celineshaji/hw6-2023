a4d71c4b471c13ad07c1c4222b07c93e8623b242
Celine Shaji <156857166+celineshaji@users.noreply.github.com>
2024-04-02 21:03:03 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -9,38 +9,31 @@ window.addEventListener("load", function() {
 
 //initialize the video element and turn off autoplay and turn off looping
 document.addEventListener('DOMContentLoaded', function() {
-    // Turn off autoplay
     video.autoplay = false;
-    // Turn off looping
     video.loop = false;
 });
 
 //play the video and update the volume information
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
-    // play video
+    console.log("Play Video");
     video.play();
-    // set volume information to the value of the volume slider
     volume.innerHTML = volumeSlider.value + '%';
 });
 
 // pause the video
 document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video");
-    // pause the video
+    console.log("Pause Video");
     video.pause();
 });
 
 // slow down, slow down the current video speed by 10% each time the button is clicked and log the new speed to the console
 document.querySelector("#slower").addEventListener("click", function() {
-	// slow down video speed by 10%
     video.playbackRate -= 0.1;
     console.log("New speed:", video.playbackRate);
 });
 
 // increase the current video speed each time the button is clicked and log the new speed to the console
 document.querySelector("#faster").addEventListener("click", function() {
-	// speed up video speed by 10%
     video.playbackRate += 0.1;
     console.log("New speed:", video.playbackRate);
 });
@@ -48,9 +41,7 @@ document.querySelector("#faster").addEventListener("click", function() {
 // advance the current video by 10 seconds, if the video length has been exceeded go back to the start of the video
 document.querySelector("#skip").addEventListener("click", function() {
 	const videoDuration = video.duration
-    // advance video time by 10 seconds
     video.currentTime += 10;
-    // set video time to 0 if the current time is equal to the duration of the video
     if (video.currentTime == videoDuration) {
         video.currentTime = 0;
     }
